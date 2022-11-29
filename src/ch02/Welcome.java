0200658936d83c89093be5ea62a204a9424775ce
puicu9 <puicu9@gmail.com>
2022-11-29 12:42:00 +0900

    merge 된 이후!

--- src/ch02/Welcome.java
+++ src/ch02/Welcome.java
@@ -3,7 +3,8 @@ package ch02;
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("welcome01");
-        System.out.println("welcome02_written by hee22");
+        System.out.println("welcome02_written by hee");
         System.out.println("welcome02");
+        System.out.println("welcome03");
     }
 }
